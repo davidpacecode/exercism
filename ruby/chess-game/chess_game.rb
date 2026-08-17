6889c41bc546b05d7d46e910505aaf9463fffb1a
davidpacecode <davidpace@gmail.com>
2026-08-16 21:16:40 -0500

    finished chess-game

---
+++ ruby/chess-game/chess_game.rb
@@ -0,0 +1,31 @@
+module Chess
+
+  RANKS = 1..8
+  FILES = 'A'..'H'
+
+  def self.valid_square?(rank, file)
+    if RANKS.include?(rank) && FILES.include?(file)
+      true
+    else
+      false
+    end
+  end
+
+  def self.nickname(first_name, last_name)
+    "#{first_name[0..1].upcase}#{last_name[-2..-1].upcase}"
+  end
+
+  def self.move_message(first_name, last_name, square)
+    if valid_square?(square[1].to_i, square[0])
+      "#{nickname(first_name, last_name)} moved to #{square}"
+    else
+      "#{nickname(first_name, last_name)} attempted to move to #{square}, but that is not a valid square"
+    end
+  end
+end
+
+puts Chess.valid_square?(1, 'A')
+
+puts Chess.nickname("John", "Doe")
+
+puts Chess.move_message("John", "Doe", "A1")
