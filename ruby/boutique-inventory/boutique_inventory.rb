45bc54a41630c747844606a04ade10535161c485
davidpacecode <davidpace@gmail.com>
2026-08-19 00:20:39 -0500

    in the midst of boutique-inventory

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -9,13 +9,13 @@ class BoutiqueInventory
   end
 
   def cheap
-    @items.map { |item|
-      item[:price].select { |price| price <= 30.0 }
-    }.flatten.sort
+    @items.select { |item|
+      item[:price] <= 30.0 }
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item|
+    item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
@@ -43,3 +43,10 @@ puts BoutiqueInventory.new([
   {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
   {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
 ]).cheap
+
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).out_of_stock
