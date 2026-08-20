807a56b1079c450a84e81895e8cc946bf216b4fd
davidpacecode <davidpace@gmail.com>
2026-08-19 23:52:32 -0500

    almost done with boutique-inventory

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -15,21 +15,28 @@ class BoutiqueInventory
 
   def out_of_stock
     @items.select { |item|
-    item[:quantity_by_size].empty? }
+      item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find { |item|
+      item[:name] == name }[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+#     @items.map { |item|
+#     item[:quantity_by_size] }.sum
+    items.map { |item|
+    item[:quantity_by_size] }.map { |qbs| qbs }.sum
   end
 
   private
   attr_reader :items
 end
 
+puts ""
+puts "1"
+puts ""
 puts BoutiqueInventory.new([
   {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
   {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
@@ -37,6 +44,9 @@ puts BoutiqueInventory.new([
   {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
 ]).item_names
 
+puts ""
+puts "2"
+puts ""
 puts BoutiqueInventory.new([
   {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
   {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
@@ -44,9 +54,32 @@ puts BoutiqueInventory.new([
   {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
 ]).cheap
 
+puts ""
+puts "3"
+puts ""
 puts BoutiqueInventory.new([
   {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
   {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
   {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
   {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
 ]).out_of_stock
+
+puts ""
+puts "4"
+puts ""
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).stock_for_item("Black Short Skirt")
+
+puts ""
+puts "5"
+puts ""
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).total_stock
