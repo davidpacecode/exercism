3b38897c288a7e802f2d19bf61294d4f3e1faab3
davidpacecode <davidpace@gmail.com>
2026-08-22 20:13:57 -0500

    finished boutique-inventory-improvements

---
+++ ruby/boutique-inventory-improvements/boutique_inventory.rb
@@ -0,0 +1,53 @@
+require 'ostruct'
+
+class BoutiqueInventory
+  attr_reader :items
+
+  def initialize(items)
+    @items = items.map { |item| OpenStruct.new(item) }
+  end
+
+  def item_names
+    @items.map(&:name).sort
+  end
+
+  def total_stock
+    @items.sum do |item|
+      item.quantity_by_size.values.sum
+    end
+  end
+end
+
+inventory = BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+])
+
+puts inventory.items.first.name
+# => "Maxi Brown Dress"
+
+puts inventory.items.first.price
+# => 65
+
+puts inventory.items.size
+# => 4
+
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).item_names
+
+# => ["Bamboo Socks Cats", "Black Short Skirt", "Maxi Brown Dress", "Red Short Skirt"]
+
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).total_stock
+
+# => 36
