6f861fe1337bb47b840bd14d8af5b873c79eb020
davidpacecode <davidpace@gmail.com>
2026-08-24 22:00:04 -0500

    completed locomotive-engineer

--- ruby/locomotive-engineer/locomotive_engineer.rb
+++ ruby/locomotive-engineer/locomotive_engineer.rb
@@ -6,12 +6,15 @@ class LocomotiveEngineer
     output_wagons_id = *temp_wagons_id[0], *missing_wagons, *temp_wagons_id[1..temp_wagons_id.length - 1]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(routing_hash, **arguments)
+    stops = {stops: arguments.values.to_a}
+    new_routing_hash = {**routing_hash, **stops}
+    new_routing_hash
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    extended_route_information = {**route, **more_route_information}
+    extended_route_information
   end
 end
 
@@ -24,3 +27,15 @@ puts ""
 puts "2"
 puts ""
 puts LocomotiveEngineer.fix_list_of_wagons([2, 5, 1, 7, 4, 12, 6, 3, 13], [3, 17, 6, 15])
+
+puts ""
+puts "3"
+puts ""
+puts LocomotiveEngineer.add_missing_stops({from: "New York", to: "Miami"},
+                    stop_1: "Washington, DC", stop_2: "Charlotte", stop_3: "Atlanta",
+                    stop_4: "Jacksonville", stop_5: "Orlando")
+
+puts ""
+puts "4"
+puts ""
+puts LocomotiveEngineer.extend_route_information({"from": "Berlin", "to": "Hamburg"}, {"length": "100", "speed": "50"})
